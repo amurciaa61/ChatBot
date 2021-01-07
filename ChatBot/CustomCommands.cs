a3fa3bd11a95ed4e19fa72d1868a84a1356f1265
Antonio Murcia <amurciaa61@gmail.com>
2021-01-07 18:00:51 +0100

    Agregar archivos de proyecto.

---
+++ ChatBot/CustomCommands.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ChatBot
+{
+    public static class CustomCommands
+    {
+        public static readonly RoutedUICommand Salir = new RoutedUICommand
+        (
+            "Salir",
+            "Salir",
+            typeof(CustomCommands),
+            new InputGestureCollection()
+            {
+                new KeyGesture(Key.F4,ModifierKeys.Alt)
+            }
+        );
+    } 
+}
